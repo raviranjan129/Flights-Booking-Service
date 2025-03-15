3baf580ba6e22c2e8e9d35fad34b1a73d08ac499
name <rk6318633@gmail.com>
2025-03-15 15:45:25 +0530

    added booking logic with transaction

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,52 +1,14 @@
-const {StatusCodes}=require('http-status-codes');
-const AppError=require('../utils/errors/app-error')
 
-
-class CrudRepository{
-    constructor(model){
-        this.model=model;
+const {Booking}=require('../models');
+const CrudRepository = require('./crud-repository');
+class BookingRepository extends CrudRepository{
+    constructor(){
+        super(Booking);
     }
-
-    async create(data){
-        const response = await this.model.create(data);
-        return response;
-    }
-
-    async destroy(data){
-        const response = await this.model.destroy({
-            where:{
-                id:data
-            }
-            
-        })
-
-        if(!response){
-            throw new AppError('Not able to find the resource ',StatusCodes.NOT_FOUND);
-        }
-        return response
-    }
-
-    async get(data){
-        const response=await this.model.findByPk(data);
-        if(!response){
-            throw new AppError('Not able to find resource',StatusCodes.NOT_FOUND);
-        }
-        return response;
-    }
-
-    async getAll(){
-        const response=await this.model.findAll();
-        return response;
-    }
-
-    async update(id,data){
-        const response = await this.model.update(data,{
-            where:{
-                id:id
-            }
-        });
+    async createBooking(data,transaction){
+        const response=await Booking.create(data,{transaction:transaction});
         return response;
     }
 }
 
-module.exports=CrudRepository;
+module.exports=BookingRepository;
