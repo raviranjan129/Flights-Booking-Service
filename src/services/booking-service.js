eb1f1ad8bd710399af789e9437618758b0b0f5a7
name <rk6318633@gmail.com>
2025-03-16 15:51:26 +0530

    added cancel mechanism

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -48,7 +48,7 @@ async function makePayment(data) {
         console.log(bookingDetails);
 
         if(bookingDetails.status==CANCELLED){
-            await bookingRepository.update(data.bookingId,{status:CANCELLED},transaction)
+           
             throw new AppError('The booking has expired ',StatusCodes.BAD_REQUEST);
         }
 
@@ -57,6 +57,8 @@ async function makePayment(data) {
         const currentTime=new Date();
 
         if(currentTime-bookingTime >300000){
+
+            await cancelBooking(data.bookingId)
             throw new AppError('The booking has expired',StatusCodes.BAD_REQUEST);
         }
 
@@ -76,6 +78,31 @@ async function makePayment(data) {
         throw error;
     }
 }
+
+
+async function cancelBooking(bookingId) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const bookingDetails = await bookingRepository.get(bookingId, transaction);
+        console.log(bookingDetails);
+        if(bookingDetails.status == CANCELLED) {
+            await transaction.commit();
+            return true;
+        }
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`, {
+            seats: bookingDetails.noOfSeats,
+            dec: 0
+        });
+        await bookingRepository.update(bookingId, {status: CANCELLED}, transaction);
+        await transaction.commit();
+
+    } catch(error) {
+        await transaction.rollback();
+        throw error;
+    }
+}
+
+
 
 
 module.exports={
